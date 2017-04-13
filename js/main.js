1ca73c560b82f5fe9b73f7ddf8f4b1af86751bf2
haojunyu <haojunyu2012@gmail.com>
2017-04-13 23:14:06 +0800

    Site update:2017-04-13 23:14:05

---
+++ js/main.js
@@ -0,0 +1,10 @@
+require([], function (){
+
+    // TOC
+    require(['toc'], function(){ })
+
+    $(document).ready(function() {
+        //var divPar = $("#comments")
+    })
+
+})
---
+++ static/code/hello.c
@@ -0,0 +1,9 @@
+///@file    hello.c
+///@author  hjy
+
+#include<stdio.h>
+
+int main(){
+    printf("Hello World!");
+    return 0;
+}
